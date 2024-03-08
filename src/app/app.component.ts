b2db6969e9247f7ed8542a49794174971380a8bb
Aldo Gordoni <2016736@swansea.ac.uk>
2024-03-08 23:47:18 +0000

    Created Initial page
    Updated to angular17
    
    created 4 different sections for informations
    
    Imported FontAwesome
    
    Added general styling
    
    Added prettier to format on save and commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'about-me';
+  faEnvelope = faEnvelope;
+  faGithub = faGithub;
+  faLinkedin = faLinkedin;
 }
